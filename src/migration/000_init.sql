f4dc54730525a394ad6a9c2cdd75e284d3c36fac
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2025-10-19 23:08:09 +0900

    fix: correct pull_request_qwen3_embedding table schema by adding diff_text column and updating comments

--- src/migration/000_init.sql
+++ src/migration/000_init.sql
@@ -1,3 +1,5 @@
-CREATE TABLE pull_request_qwen3_embedding(pull_request_id, diff_embedding float[4096], PRIMARY KEY(pull_request_id));
-COMMENT ON TABLE pull_request IS 'pull request metadata';
-COMMENT ON COLUMN pull_request.id IS 'id of pull request';
+CREATE TABLE pull_request_qwen3_embedding(pull_request_id bigint, diff_text varchar, diff_embedding float[4096], PRIMARY KEY(pull_request_id));
+COMMENT ON TABLE pull_request_qwen3_embedding IS 'pull request metadata';
+COMMENT ON COLUMN pull_request_qwen3_embedding.pull_request_id IS 'id of pull request';
+COMMENT ON COLUMN pull_request_qwen3_embedding.diff_text IS 'diff text of pull request';
+COMMENT ON COLUMN pull_request_qwen3_embedding.diff_embedding IS 'diff embedding of pull request';
