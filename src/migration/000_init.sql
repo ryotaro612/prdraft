cb1c450589d6da173e77acd32e1f98583bdb4255
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2025-10-21 01:51:27 +0900

    feat: update .gitignore and improve create_pull_request.toml prompt; refactor State initialization and enhance find_similar_pull_requests functionality

--- src/migration/000_init.sql
+++ src/migration/000_init.sql
@@ -3,3 +3,6 @@ COMMENT ON TABLE pull_request_qwen3_embedding IS 'pull request metadata';
 COMMENT ON COLUMN pull_request_qwen3_embedding.pull_request_id IS 'id of pull request';
 COMMENT ON COLUMN pull_request_qwen3_embedding.diff_text IS 'diff text of pull request';
 COMMENT ON COLUMN pull_request_qwen3_embedding.diff_embedding IS 'diff embedding of pull request';
+
+
+create table pull_request(id bigint, body varchar, raw json, primary key(id));
--- src/onagigawa/mcp.py
+++ src/onagigawa/mcp.py
@@ -18,7 +18,7 @@ class Args(typing.Protocol):
 
 class State:
 
-    def __init__(self, repository: git.Repo, db: duckdb.DuckDBPyConnection):
+    def __init__(self, repository: git.Repo, db: str):
         self.repository = repository
         self.model = OllamaEmbeddings(model="qwen3-embedding:8b")
         self.db = db
@@ -36,21 +36,40 @@ def find_similar_pull_requests(base: str, head: str) -> str:
     text = r.make_diff_summary(state.repository, base, head)
 
     embedding = state.model.embed_documents([text])[0]
+    conn = duckdb.connect(state.db, read_only=True)
 
-    res = state.db.execute(
+    res = conn.execute(
         "select pull_request_id, diff_text from pull_request_qwen3_embedding "
-        "ORDER BY array_cosine_distance(diff_embedding, $embedding) limit 5",
+        "ORDER BY array_cosine_distance(diff_embedding, $embedding::FLOAT[4096]) limit 15",
         {"embedding": embedding},
-    )
+    ).fetchall()
 
-    return ", ".join([record[0] for record in res.fetchall()])
+    ids = [record[0] for record in res]
+
+    similar_texts = [
+        r[1]
+        for r in conn.execute(
+            "select id, body from pull_request where id in $ids", {"ids": ids}
+        ).fetchall()
+    ]
+    return "\n\n---\n\n".join(similar_texts)
+
+
+@mcp.tool(
+    name="make_pull_request_content",
+    description="Make pull request content based on code changes and commit messages.",
+)
+def make_pull_request_content(base: str, head: str) -> str:
+
+    text = r.make_diff_summary(state.repository, base, head)
+
+    return text
 
 
 def run(args: Args) -> int:
     global state
     repo = git.Repo(args.repository)
-    db = duckdb.connect(args.db)
-    state = State(repo, db)
+    state = State(repo, args.db)
     mcp.run()
     # https://github.com/modelcontextprotocol/python-sdk?tab=readme-ov-file#direct-execution
     return 0
