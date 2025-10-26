177c5009b905e51e321988ed47ab7fb26d74fae6
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2025-10-27 00:27:19 +0900

    feat: refactor pull request handling and enhance response processing

--- src/prdraft/pullrequest/github.py
+++ src/prdraft/pullrequest/github.py
@@ -1,10 +1,52 @@
 import requests
 
 
-def get_pull_request(
+class PullRequest:
+
+    def __init__(self, raw: dict):
+        self._raw = raw
+
+    @property
+    def title(self) -> str:
+        return self._raw["title"]
+
+    @property
+    def id(self) -> int:
+        return self._raw["id"]
+
+    @property
+    def number(self) -> int:
+        return self._raw["number"]
+
+    @property
+    def merged(self) -> bool:
+        """"""
+        return True if self._raw.get("merged_at", False) else False
+
+    @property
+    def head_sha(self) -> str:
+        """"""
+        return self._raw["head"]["sha"]
+
+    @property
+    def base_sha(self) -> str:
+        """"""
+        return self._raw["base"]["sha"]
+
+
+class PullRequests:
+
+    def __init__(self, response: requests.models.Response):
+        self._response = response
+
+    def pull_requests(self) -> list[PullRequest]:
+        return [PullRequest(item) for item in self._response.json()]
+
+
+def get_pull_requests(
     org: str, repo: str, github_api_token: str, offset: int, size: int
-) -> requests.models.Response:
-    return requests.get(
+) -> requests.models.Response | PullRequests:
+    resp = requests.get(
         f"https://api.github.com/repos/{org}/{repo}/pulls",
         params={"state": "all", "direction": "asc", "page": offset, "per_page": size},
         headers={
@@ -13,3 +55,7 @@ def get_pull_request(
             "X-GitHub-Api-Version": "2022-11-28",
         },
     )
+    if resp.status_code != 200:
+        return resp
+
+    return PullRequests(resp)
