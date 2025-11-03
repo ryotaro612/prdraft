f97785e6363bf7546b9e2b18df40d017d6a7940b
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2025-11-03 18:00:33 +0900

    refactor: optimize make_summary function and improve commit message selection logic

--- src/prdraft/pullrequest/summary.py
+++ src/prdraft/pullrequest/summary.py
@@ -12,18 +12,18 @@ class TokenCounter(typing.Protocol):
 def make_summary(
     repo: git.Repo, base: str, head: str, tokenizer: TokenCounter, token_limit: int
 ):
-    current_tokens = 0
-    commit_messages = []
-    for message in _get_commit_messages(repo, base, head):
-        message_tokens = tokenizer.count_tokens(message)
-        if current_tokens + message_tokens > token_limit:
-            break
-        commit_messages.append(message)
-        current_tokens += message_tokens
+    commit_messages, current_tokens = _select_commit_messages(
+        repo, base, head, tokenizer, token_limit
+    )
 
+    # T, U changes do not exist in pull request diff.
+    # https://git-scm.com/docs/git-diff/2.19.0#Documentation/git-diff.txt---diff-filterACDMRTUXB
     change_types: list[typing.Literal["A", "D", "C", "M", "R", "T", "U"] | None] = [
         diff.change_type for diff in repo.commit(base).diff(repo.commit(head))
     ]
+    # d -> only a, diff exists
+    # a-> b only diff exits
+    # c -> hasn't seend yet
 
     diffs = repo.commit(base).diff(repo.commit(head), create_patch=True)
 
@@ -32,20 +32,15 @@ def make_summary(
     deleted: list[_CountDiff] = []
     copied: list[_CountDiff] = []
     for change_type, diff in zip(change_types, diffs):
+
         n_a_path = 0
         if diff.a_path:
             n_a_path = tokenizer.count_tokens(diff.a_path)
         n_b_path = 0
         if diff.b_path:
             n_b_path = tokenizer.count_tokens(diff.b_path)
-        n_a_mode = 0
-        if diff.a_mode:
-            n_a_mode = tokenizer.count_tokens(str(diff.a_mode))
-        n_b_mode = 0
-        if diff.b_mode:
-            n_b_mode = tokenizer.count_tokens(str(diff.b_mode))
 
-        count_diff = _CountDiff(n_a_path, n_a_mode, n_b_path, n_b_mode, diff)
+        count_diff = _CountDiff(n_a_path, n_b_path, diff)
 
         if change_type == "A":
             added.append(count_diff)
@@ -60,127 +55,102 @@ def make_summary(
     meta_deleted: list[_CountDiff] = []
     meta_modified: list[_CountDiff] = []
 
-    for que, lst in zip(
+    for meta_lst, lst in zip(
         [meta_added, meta_modified, meta_deleted, meta_copied],
         [added, modified, deleted, copied],
     ):
         for item in lst:
-            if item.total_path_mode_token() + current_tokens > token_limit:
+            if item.sum_path_tokens() + current_tokens > token_limit:
                 break
-            current_tokens += item.total_path_mode_token()
-            que.append(item)
+            current_tokens += item.sum_path_tokens()
+            meta_lst.append(item)
+
     diff_added: list[_CountDiff] = []
     diff_copied: list[_CountDiff] = []
     diff_deleted: list[_CountDiff] = []
     diff_modified: list[_CountDiff] = []
+
     for diff_lst, meta_lst in zip(
         [diff_added, diff_modified, diff_deleted, diff_copied],
         [meta_added, meta_modified, meta_deleted, meta_copied],
     ):
         temp = list(meta_lst)
         meta_lst.clear()
-        for a in temp:
-            text = a.diff_text()
+        for count_diff in temp:
+            text = count_diff.diff_text()
             if text is None:
-                meta_lst.append(a)
+                meta_lst.append(count_diff)
             else:
                 text_tokens = tokenizer.count_tokens(text)
                 if current_tokens + text_tokens > token_limit:
-                    meta_lst.append(a)
+                    meta_lst.append(count_diff)
                 else:
                     current_tokens += text_tokens
-                    diff_lst.append(a)
-    summary_md = "# Pull request summary\n\n"
+                    diff_lst.append(count_diff)
+
+    summary_md = "# Pull request summary\n"
     if meta_copied or diff_copied:
-        summary_md += "## Copied files\n\n"
-        for d in diff_copied:
-            summary_md += f"from: {d.diff.a_path}(mode: {d.diff.a_mode}) to: {d.diff.b_path}(mode: {d.diff.b_mode})\n\n"
-            text = d.diff_text()
-            if text is None:
-                raise ValueError("diff text is None")
-            summary_md += "diff:\n\n"
-            for line in text.splitlines():
-                summary_md += f"    {line}\n"
-            summary_md += "\n"
-        for d in meta_copied:
-            summary_md += f"from: {d.diff.a_path}(mode: {d.diff.a_mode}) to: {d.diff.b_path}(mode: {d.diff.b_mode})\n"
-        summary_md += "\n"
+        raise ValueError("copied files are not supported yet")
+
     if meta_deleted or diff_deleted:
-        summary_md += "## Deleted files\n\n"
-        for d in diff_copied:
-            summary_md += f"path: {d.diff.a_path} mode: {d.diff.a_mode}\n\n"
+        summary_md += "## Deleted files\n"
+        for d in diff_deleted:
+            summary_md += f"path: {d.diff.a_path}\n"
             text = d.diff_text()
             if text is None:
-                raise ValueError("diff text is None")
+                raise ValueError("diff text does not exist")
             summary_md += "diff:\n\n"
             for line in text.splitlines():
                 summary_md += f"    {line}\n"
-            summary_md += "\n"
-        for d in meta_copied:
-            summary_md += f"path: {d.diff.a_path} mode: {d.diff.a_mode}\n"
-        summary_md += "\n"
+        for d in meta_deleted:
+            summary_md += f"path: {d.diff.a_path}\n"
 
     if meta_added or diff_added:
-        summary_md += "## Added files\n\n"
+        summary_md += "## Added files\n"
         for d in diff_added:
-            summary_md += f"path: {d.diff.b_path} mode: {d.diff.b_mode}\n\n"
+            summary_md += f"path: {d.diff.b_path}\n"
             text = d.diff_text()
             if text is None:
-                raise ValueError("diff text is None")
+                raise ValueError("diff text does not exist")
             summary_md += "diff:\n\n"
             for line in text.splitlines():
                 summary_md += f"    {line}\n"
-            summary_md += "\n"
         for d in meta_added:
-            summary_md += f"path: {d.diff.b_path} mode: {d.diff.b_mode}\n"
-        summary_md += "\n"
+            summary_md += f"path: {d.diff.b_path}\n"
 
     if meta_modified or diff_modified:
-        summary_md += "## Modified files\n\n"
+        summary_md += "## Modified files\n"
         for d in diff_modified:
-            if d.diff.a_path and d.diff.b_path and d.diff.a_path != d.diff.b_path:
-                headline = f"from: {d.diff.a_path}(mode: {d.diff.a_mode}) to: {d.diff.b_path}(mode: {d.diff.b_mode})\n"
-            elif d.diff.a_path:
-                headline = f"path: {d.diff.a_path} mode: {d.diff.a_mode}\n"
-            else:
-                raise ValueError("both a_path and b_path are None")
-            summary_md += headline + "\n"
+            if d.diff.a_path != d.diff.b_path:
+                raise ValueError("Unexpected path change in modified files")
+            summary_md += f"path: {d.diff.a_path}\n"
             text = d.diff_text()
             if text is None:
-                raise ValueError("diff text is None")
+                raise ValueError("diff text does not exist")
             summary_md += "diff:\n\n"
             for line in text.splitlines():
                 summary_md += f"    {line}\n"
-            summary_md += "\n"
         for d in meta_modified:
-            if d.diff.a_path and d.diff.b_path and d.diff.a_path != d.diff.b_path:
-                summary_md += f"from: {d.diff.a_path}(mode: {d.diff.a_mode}) to: {d.diff.b_path}(mode: {d.diff.b_mode})\n"
-            elif d.diff.a_path:
-                summary_md += f"path: {d.diff.a_path} mode: {d.diff.a_mode}\n"
-            else:
-                raise ValueError("both a_path and b_path are None")
-        summary_md += "\n"
+            if d.diff.a_path != d.diff.b_path:
+                raise ValueError("Unexpected path change in modified files")
+            summary_md += f"path: {d.diff.a_path}\n"
 
-    summary_md += "## Commit messages\n\n"
-    for msg in commit_messages:
+    summary_md += "## Commit messages\n"
+    for i, msg in enumerate(commit_messages):
+        summary_md += f"### Commit {i + 1}\n"
         for line in msg.splitlines():
             summary_md += f"    {line}\n"
-        summary_md += "\n\n"
     return summary_md
 
 
 class _CountDiff:
-    def __init__(
-        self, n_a_path: int, n_a_mode: int, n_b_path: int, n_b_mode: int, diff: git.Diff
-    ):
+    def __init__(self, n_a_path: int, n_b_path: int, diff: git.Diff):
         self.n_a_path = n_a_path
-        self.n_a_mode = n_a_mode
         self.n_b_path = n_b_path
-        self.n_b_mode = n_b_mode
         self.diff = diff
 
-    def total_path_mode_token(self) -> int:
-        return self.n_a_path + self.n_a_mode + self.n_b_path + self.n_b_mode
+    def sum_path_tokens(self) -> int:
+        return self.n_a_path + self.n_b_path
 
     def diff_text(self) -> str | None:
         return self._to_str(self.diff.diff)
@@ -205,10 +175,17 @@ def _get_commit_messages(repo: git.Repo, start_sha: str, end_sha: str) -> list[s
     return messages
 
 
-class _Summary:
+def _select_commit_messages(
+    repo: git.Repo, base: str, head: str, tokenizer: TokenCounter, token_limit: int
+) -> typing.Tuple[list[str], int]:
+    current_tokens = 0
+    commit_messages: list[str] = []
 
-    def __init__(self, messages: list[str]):
-        self.messages = messages
+    for message in _get_commit_messages(repo, base, head):
+        message_tokens = tokenizer.count_tokens(message)
+        if current_tokens + message_tokens > token_limit:
+            break
+        commit_messages.append(message)
+        current_tokens += message_tokens
 
-
-# https://git-scm.com/docs/git-diff/2.19.0#Documentation/git-diff.txt---diff-filterACDMRTUXB
+    return commit_messages, current_tokens
